576517c56f17c2ee61e661b3626332c90884491d
Marcin Zieba <marcinpsk@gmail.com>
2026-09-01 11:40:17 +0200

    test: address follow-up PR 128 review

--- netbox_librenms_plugin/tests/test_coverage_virtual_chassis.py
+++ netbox_librenms_plugin/tests/test_coverage_virtual_chassis.py
@@ -74,65 +74,6 @@ class TestCreateVirtualChassisWithMembers:
         assert virtual_chassis.domain == f"librenms-{master.pk}"
 
 
-@pytest.mark.django_db
-class TestLoadVcMemberNamePattern:
-    DEFAULT = "-M{position}"
-
-    @staticmethod
-    def _call():
-        from netbox_librenms_plugin.import_utils.virtual_chassis import _load_vc_member_name_pattern
-
-        return _load_vc_member_name_pattern()
-
-    @staticmethod
-    def _set_pattern(value):
-        from netbox_librenms_plugin.models import LibreNMSSettings
-
-        settings, _ = LibreNMSSettings.objects.get_or_create(pk=1)
-        settings.vc_member_name_pattern = value
-        settings.save()
-
-    def test_returns_configured_pattern(self):
-        self._set_pattern("-SW{position}")
-        assert self._call() == "-SW{position}"
-
-    @pytest.mark.parametrize("value", ["", "   "])
-    def test_blank_pattern_uses_default(self, value):
-        self._set_pattern(value)
-        assert self._call() == self.DEFAULT
-
-    def test_missing_settings_uses_default(self):
-        from netbox_librenms_plugin.models import LibreNMSSettings
-
-        LibreNMSSettings.objects.all().delete()
-        assert self._call() == self.DEFAULT
-
-
-@pytest.mark.django_db
-class TestGenerateVcMemberName:
-    @staticmethod
-    def _call(master_name, position, serial=None, pattern=None):
-        from netbox_librenms_plugin.import_utils.virtual_chassis import _generate_vc_member_name
-
-        return _generate_vc_member_name(master_name, position, serial=serial, pattern=pattern)
-
-    def test_explicit_pattern_and_serial_are_formatted(self):
-        assert self._call("switch01", 2, serial="ABC123", pattern="-SW{position}-{serial}") == "switch01-SW2-ABC123"
-
-    def test_none_pattern_reads_real_settings(self):
-        from netbox_librenms_plugin.models import LibreNMSSettings
-
-        settings, _ = LibreNMSSettings.objects.get_or_create(pk=1)
-        settings.vc_member_name_pattern = "-STACK{position}"
-        settings.save()
-
-        assert self._call("core01", 3) == "core01-STACK3"
-
-    @pytest.mark.parametrize("pattern", ["{position!z}", "-{unknown_key}"])
-    def test_malformed_pattern_uses_default(self, pattern):
-        assert self._call("switch01", 2, pattern=pattern) == "switch01-M2"
-
-
 class TestNormSerial:
     @pytest.mark.parametrize(
         ("value", "expected"),
